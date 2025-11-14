eb3ef839988556921ee6e0c9918f05ca36366074
giwon98-nam <giwon98.nam@samsung.com>
2025-11-14 11:01:25 +0900

    Fix AI-Lite advertiser to use public address

--- apps/examples/ble_rmc/ble_rmc_main.c
+++ apps/examples/ble_rmc/ble_rmc_main.c
@@ -1146,7 +1146,7 @@ int ble_rmc_main(int argc, char *argv[])
 //			uint8_t adv_event_prop = 0x1;//extended adv   
 			uint8_t adv_event_prop = 0x13;//legacy adv   
 			uint32_t adv_interval_int[2] = {32,32};
-			uint8_t own_addr_type = 1;
+			uint8_t own_addr_type = 0;
  			int16_t val = 0x7f;
 			if (adv_handle == 0){
 				ret = ble_server_create_multi_adv(adv_event_prop,
